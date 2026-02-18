cfc02302e1f3467fcc6056ea44aa1f1ee93f2b20
nassaigael <hei.nassai@gmail.com>
2026-02-18 11:16:14 +0300

    feat(db): add PostgreSQL JDBC driver dependency and enhance DBConnection class

--- src/main/java/school/hei/DBConnection.java
+++ src/main/java/school/hei/DBConnection.java
@@ -1,4 +1,15 @@
 package school.hei;
 
+import java.sql.*;
+
 public class DBConnection {
+
+
+    public Connection getConnection() throws SQLException {
+        final String userName = "push_down_user";
+        final String password = "123456";
+        final  String url = "jdbc:postgresql://localhost:5432/push_down_db";
+
+        return DriverManager.getConnection(url, userName, password);
+    }
 }
